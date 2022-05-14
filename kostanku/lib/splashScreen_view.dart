8f8a4eef4448672c857681c0d45a331cee5ce911
tustiantifany <tustiantifany@gmail>
2022-05-14 07:48:17 +0700

    -

---
+++ kostanku/lib/splashScreen_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'HomePage.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    startSplashScreen();
+  }
+
+  startSplashScreen() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) {
+          return HomePage();
+        }),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          "images/logo.png",
+          width: 300.0,
+          height: 300.0,
+        ),
+      ),
+    );
+  }
+}

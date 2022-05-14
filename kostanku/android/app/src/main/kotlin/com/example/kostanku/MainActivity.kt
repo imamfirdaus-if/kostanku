60e6e5f2182ec7fcbb6a7252b83123d91c9fdd23
tustiantifany <tustiantifany@gmail>
2022-05-14 08:00:30 +0700

    -

---
+++ kostanku/android/app/src/main/kotlin/com/example/kostanku/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kostanku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ kostanku/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:kostanku/splashScreen_view.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreenPage(),
+  ));
+}

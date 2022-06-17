f765a9650b2f95ec2f5efa90efcde66e3f692ab9
imamfirdaus-if <imamfirdaus3110@gmail.com>
2022-06-17 15:11:22 +0700

    loggin session

--- kostanku/lib/main.dart
+++ kostanku/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kostanku/modules/splashscreen/splashscreen_view.dart';
@@ -13,7 +14,9 @@ class MyBehavior extends ScrollBehavior {
   }
 }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
